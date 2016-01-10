13bbad7c8cff1f1cf2acbdc9659557df38df1675
Hyeunjoon Kim <hyeunjoon.kim@mail.mcgill.ca>
2016-01-09 22:04:25 -0800

    added mail feature

--- src/components/signupform.jsx
+++ src/components/signupform.jsx
@@ -9,12 +9,18 @@ module.exports = React.createClass({
 			backgroundColor: '',
 			color: '',
 			count: 0,
+			business_name: "",
+			business_type: "",
+			business_location: "",
+			contact_email: "",
+			contact_phone: ""
 		})
 	},
 
 	toggleFormOpen: function() {
 		if (this.state.formshow) {
 			this.setState({formshow: false, height: 40, width: 50, backgroundColor: '#1F2938',color: '#DADEE4'})
+			this.sendMail();
 		} else {
 			this.setState({formshow: true, height: 330, width: 100, backgroundColor: '#939598', color: '#1F2938'})
 		}
@@ -31,6 +37,16 @@ module.exports = React.createClass({
 		}
 	},
 
+	sendMail: function (){
+		console.log("send mail triggered!");
+		emailjs.send("gmail", "test", {"business_name" : this.state.business_name, "business_type" : this.state.business_type, "business_location" : this.state.business_location, "contact_email" : this.state.contact_email, "contact_phone" : this.state.contact_phone})
+	},
+
+	handleChange(event){
+		var input = {}
+		input[event.target.dataset.type] = event.target.value
+		this.setState(input);
+	},
 
 	render: function(){
 
@@ -45,23 +61,23 @@ module.exports = React.createClass({
 					<div id='scrollable-frame'>
 						<div id='name'>
 							<div className='title'> Name of your business</div>
-							<input type='text' placeholder='Eg: Malik Law Corp.'></input>
+							<input data-type="business_name" onChange={this.handleChange} type='text' placeholder='Eg: Malik Law Corp.'></input>
 						</div>
 						<div id='type'>
 							<div className='title'> Type of your business </div>
-							<input type='text' placeholder='Eg: Civil Lawyer'/> 
+							<input data-type="business_type" onChange={this.handleChange} type='text' placeholder='Eg: Civil Lawyer'/> 
 						</div>
 						<div id='email'>
 							<div className='title'> Location of your business </div>
-							<input type='text' placeholder='Eg: 129th Street Surrey, BC, V3W 4G2'></input>
+							<input data-type="business_location" onChange={this.handleChange} type='text' placeholder='Eg: 129th Street Surrey, BC, V3W 4G2'></input>
 						</div>
 						<div id='email'>
 							<div className='title'>Contact Email </div>
-							<input type='text' placeholder='Eg: hello123@gmail.com'></input>
+							<input data-type="contact_email" onChange={this.handleChange} type='text' placeholder='Eg: hello123@gmail.com'></input>
 						</div>
 						<div id='type'>
 							<div className='title'> Contact phone number </div>
-							<input type='text' placeholder='Eg: 604-123-4567'/> 
+							<input data-type="contact_phone" onChange={this.handleChange} type='text' placeholder='Eg: 604-123-4567'/> 
 						</div>
 						<div onClick={this.toggleFormOpen} id='submit'>
 							<span> Submit </span>
